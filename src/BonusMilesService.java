4ba4906222cad8b90717b397a21328f8b3022fde
Павел Воронин <nordfox88@gmail.com>
2022-08-07 18:12:13 +0300

    update

---
+++ src/BonusMilesService.java
@@ -0,0 +1,12 @@
+public class BonusMilesService {
+
+    int calculate (int cost) {
+        int result;
+        int bonus = 20;
+        System.out.println("вы получите бонусных миль:");
+        result = (cost/bonus);
+
+        return result;
+
+    }
+}
